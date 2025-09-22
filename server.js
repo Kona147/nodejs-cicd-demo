ccf93f1fcaef3ba882cc2f488b71ac03228d4807
Kona147 <100594747+Kona147@users.noreply.github.com>
2025-09-22 23:44:58 +0530

    Initial commit - Node.js CI/CD demo

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    "🚀 Hello from Node.js CI/CD Pipeline with GitHub Actions + Docker (Alpine)!"
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
